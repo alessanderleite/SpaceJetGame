99169463b3f51eb1795fdd7bb9a3b0ac8316f5dc
alessanderleite <alessander.logistica@gmail.com>
2019-01-13 10:09:56 -0200

    new class called GameView

---
+++ app/src/main/java/br/com/alessanderleite/spacejetgame/GameView.java
@@ -0,0 +1,15 @@
+package br.com.alessanderleite.spacejetgame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+    public GameView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
