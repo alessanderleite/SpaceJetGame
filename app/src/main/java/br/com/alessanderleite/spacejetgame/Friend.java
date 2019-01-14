512908d49fd86c0fdc8b3b481184856f8a2f6f39
alessanderleite <alessander.logistica@gmail.com>
2019-01-14 21:58:31 -0200

    declare the Bitmap object and the variables inside the Friend class

---
+++ app/src/main/java/br/com/alessanderleite/spacejetgame/Friend.java
@@ -0,0 +1,18 @@
+package br.com.alessanderleite.spacejetgame;
+
+import android.graphics.Bitmap;
+
+public class Friend {
+
+    private Bitmap bitmap;
+    private int x;
+    private int y;
+    private int speed = 1;
+
+    private int maxX;
+    private int minX;
+
+    private int maxY;
+    private int minY;
+    
+}
