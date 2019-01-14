d97043bd16846791de074aa79a1e71560f1bf858
alessanderleite <alessander.logistica@gmail.com>
2019-01-13 23:05:35 -0200

    declare the object, variables and the Enemy constructor

--- app/src/main/java/br/com/alessanderleite/spacejetgame/Enemy.java
+++ app/src/main/java/br/com/alessanderleite/spacejetgame/Enemy.java
@@ -1,4 +1,46 @@
 package br.com.alessanderleite.spacejetgame;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
+
 public class Enemy {
+
+    // bitmap for the enemy
+    // we have already pasted the bitmap in the drawable folder
+    private Bitmap bitmap;
+
+    // x and y coordinates
+    private int x;
+    private int y;
+
+    // enemy speed
+    private int speed = -1;
+
+    // min and max coordinates to keep the enemy inside the screen
+    private int maxX;
+    private int minX;
+
+    private int maxY;
+    private int minY;
+
+    public Enemy(Context context, int screenX, int screenY) {
+        // getting bitmap from drawable resource
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+
+        // initializing min and max coordinates
+        maxX = screenX;
+        maxY = screenY;
+        minX = 0;
+        minY = 0;
+
+        // generating a random coordinate to add enemy
+        Random generator = new Random();
+        speed = generator.nextInt(6) + 10;
+        x = screenX;
+        y = generator.nextInt(maxY) - bitmap.getHeight();
+    }
+
 }
