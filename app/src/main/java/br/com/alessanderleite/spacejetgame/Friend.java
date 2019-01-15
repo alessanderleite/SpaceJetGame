72d97305330433c7eee4c216ebee71deab3ffe76
alessanderleite <alessander.logistica@gmail.com>
2019-01-14 22:23:15 -0200

    declare the Friend constructor, the update and the getters methods

--- app/src/main/java/br/com/alessanderleite/spacejetgame/Friend.java
+++ app/src/main/java/br/com/alessanderleite/spacejetgame/Friend.java
@@ -1,6 +1,11 @@
 package br.com.alessanderleite.spacejetgame;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import java.util.Random;
 
 public class Friend {
 
@@ -14,5 +19,45 @@ public class Friend {
 
     private int maxY;
     private int minY;
-    
+
+    public Friend(Context context, int screenX, int screenY) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.friend);
+        maxX = screenX;
+        maxY = screenY;
+        minX = 0;
+        minY = 0;
+        Random generator = new Random();
+        speed = generator.nextInt(6) + 10;
+        x = screenX;
+        y = generator.nextInt(maxY) - bitmap.getHeight();
+    }
+
+    public void update(int playerSpeed) {
+        x -= playerSpeed;
+        x -= speed;
+        if (x < minX - bitmap.getWidth()) {
+            Random generator = new Random();
+            speed = generator.nextInt(10) + 10;
+            x = maxX;
+            y = generator.nextInt(maxY) - bitmap.getHeight();
+        }
+    }
+
+    // one more getter for getting the rect object
+    public Rect getDetectCollision() {
+        return getDetectCollision();
+    }
+
+    // getters
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
 }
