411398509c2c695b79650e4f3777ad4b09242c44
alessanderleite <alessander.logistica@gmail.com>
2019-01-16 07:31:28 -0200

    declare the ImageButton object for highscore, initializing in the constructor and set inside the onClick method to the MainActivity

--- app/src/main/java/br/com/alessanderleite/spacejetgame/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/spacejetgame/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     // image button
     private ImageButton buttonPlay;
 
+    // high score button
+    private ImageButton buttonScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // getting the button
         buttonPlay = (ImageButton) findViewById(R.id.buttonPlay);
 
+        // initializing the highscore button
+        buttonScore = (ImageButton) findViewById(R.id.buttonPlay);
+
+        // setting the on click listener to high score button
+        buttonScore.setOnClickListener(this);
+
         // adding a click listener
         buttonPlay.setOnClickListener(this);
     }
@@ -30,7 +39,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        // starting game activity
-        startActivity(new Intent(this, GameActivity.class));
+        if (v == buttonPlay) {
+
+            // the transition from MainActivity to GameActivity
+            startActivity(new Intent(this, GameActivity.class));
+        }
+        if (v == buttonScore) {
+
+            // the transition from MainActivity to HighScore activity
+            startActivity(new Intent(MainActivity.this, HighScore.class));
+        }
     }
 }
