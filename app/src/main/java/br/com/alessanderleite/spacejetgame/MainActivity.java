74b678d47d5a4fcc0ffd86a73eb2b16fea7af891
alessanderleite <alessander.logistica@gmail.com>
2019-01-17 07:50:13 -0200

    add the onBackPressed method to the MainActivity

--- app/src/main/java/br/com/alessanderleite/spacejetgame/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/spacejetgame/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.alessanderleite.spacejetgame;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -49,5 +51,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // the transition from MainActivity to HighScore activity
             startActivity(new Intent(MainActivity.this, HighScore.class));
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are yout sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        GameView.stopMusic();
+                        Intent startMain = new Intent(Intent.ACTION_MAIN);
+                        startMain.addCategory(Intent.CATEGORY_HOME);
+                        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(startMain);
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
