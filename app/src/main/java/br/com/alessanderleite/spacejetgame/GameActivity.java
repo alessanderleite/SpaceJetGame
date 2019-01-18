2aea2dbb0c83ca5d7d660dff90b3757098f5c989
alessanderleite <alessander.logistica@gmail.com>
2019-01-17 23:22:49 -0200

    declare the onBackPressed method to the GameActivity class

--- app/src/main/java/br/com/alessanderleite/spacejetgame/GameActivity.java
+++ app/src/main/java/br/com/alessanderleite/spacejetgame/GameActivity.java
@@ -1,5 +1,8 @@
 package br.com.alessanderleite.spacejetgame;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -42,5 +45,32 @@ public class GameActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         gameView.resume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        GameView.stopMusic();
+                        Intent starMain = new Intent(Intent.ACTION_MAIN);
+                        starMain.addCategory(Intent.CATEGORY_HOME);
+                        starMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(starMain);
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
